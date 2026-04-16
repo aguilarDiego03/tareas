43646e8f82f1ff24b97258bdcc76371292819543
aguilarDiego03 <23308060610084@cetis61.edu.mx>
2026-04-16 11:08:12 -0500

    commit

--- src/models/schemasModel.py
+++ src/models/schemasModel.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
-from datetime import date, time
+from datetime import datetime
 
 class UsuarioSchema(BaseModel):
     nombre: str = Field(min_length=3, max_length=100)
@@ -10,5 +10,5 @@ class UsuarioSchema(BaseModel):
 class TareaSchema(BaseModel):
     titulo: str = Field(min_length=1, max_length=200)
     descripcion: Optional[str] = None
-    prioridad: Optional[str] = "meedia"
-    clasificaion: str = "personal" 
+    prioridad: str = "media"
+    clasificacion: str = "personal"
